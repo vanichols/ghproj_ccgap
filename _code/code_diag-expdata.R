99c485e83987e9b87b3745468b501be79b29fe77
vanichols <vnichols@iastate.edu>
2020-03-20 13:08:13 -0500

    clean up diag-expdata

--- _code/code_diag-expdata.R
+++ _code/code_diag-expdata.R
@@ -1,5 +1,6 @@
 # Created:       2/17/2020
 # last edited:   3/16/2020
+#                3/20/2020
 # 
 # purpose: diagnose john sawyer's data
 #
@@ -8,7 +9,7 @@
 
 rm(list = ls())
 #devtools::install_github("vanichols/saapsim", force = T)
-library(saapsim) #--my package, has his data in it :|
+library(saapsim) #--my package, has his data in it :P
 library(tidyverse)
 library(lme4)
 
@@ -99,7 +100,6 @@ figloc <-
   geom_point() +
   coord_flip() + 
   labs(title = '2006-2016',
-       x = "location",
        y = "yield gap")
   
 figyr <- 
@@ -111,7 +111,8 @@ figyr <-
   geom_boxplot() + 
   geom_point() +
   coord_flip() + 
-  labs(x = NULL,
+  labs(title = "7 sites",
+       x = NULL,
        y = "yield gap")
   
 
@@ -119,6 +120,42 @@ library(patchwork)
 
 figloc / figyr
 
+
+
+# how to quantify variation explained by year vs site? --------------------
+
+
+#--assign site and year as random effects?
+library(lme4)
+library(broom)
+d_gaps
+m1 <- lmer(cgap_max ~ 1 + (1|site) + (1|year), data = d_gaps)
+summary(m1)
+
+m1err <- 
+  tidy(m1) %>% 
+  filter(term != "(Intercept)") %>% 
+  select(group, estimate)
+
+#--can I include their interaction? Does it even matter?
+
+#--look at variance 'components' (?)
+m1err %>% 
+  ggplot(aes(x = "", y = estimate, fill = group)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0) + 
+  theme_bw() +
+  labs(fill = NULL,
+       x = NULL,
+       y = NULL,
+       title = "Variance Components") + 
+  theme(axis.text = element_blank())
+
+#--what if I assume they are fixed effects?
+m2 <- lm(cgap_max ~ site*year, data = d_gaps)
+anova(m2)
+
+#--nothing is even significant. 
 
 # are yield gpas at min and max N rates related? --------------------------
 
@@ -131,120 +168,3 @@ d_gaps %>%
   labs(x = "Yield gap at 0 Nrate (kg/ha)", 
        y = "Yield gap at max Nrate (kg/ha)") + 
   theme_bw()
-
-
-
-###############old#################################
-
-# is yield gap related to average yield? --------------------------------------
-
-# slightly. Gentry found it's related to the yield gap at 0
-sad_tidysawyer %>% 
-  group_by(site, year) %>% 
-  mutate(oa_kgha = mean(yield_kgha, na.rm = T)) %>% 
-  left_join(cpen) %>% 
-  filter(!is.na(cpen)) %>% 
-  ggplot(aes(oa_kgha/1000, cpen/1000)) + 
-  geom_point() + 
-  geom_smooth(method = "lm", se = F) + 
-  labs(x = "Site-year average yield", 
-       y = "Corn Penalty (Mg/ha)")
-
-# mono-pen at 0N vs mono-pen at highest N, no.
-cpen0 %>% 
-  select(crop, site, year, cpen0) %>% 
-  left_join(cpen) %>% 
-  ggplot(aes(cpen0/1000, cpen/1000)) + 
-  geom_point() +
-  geom_smooth(method = "lm", se = F) + 
-  labs(x = "Mono-pen at 0N", 
-       y = "Mono-pen at HN (Mg/ha)")
-  
-
-sad_tidysawyer %>% select(site, year) %>% table()
-
-# take average yield at highest N rate instead?
-sad_tidysawyer %>% 
-  filter(nrate_kgha == max(nrate_kgha)) %>% 
-  group_by(crop, site, year) %>% 
-  mutate(myield = mean(yield_kgha, na.rm = T)) %>% 
-  ggplot(aes(myield, yield_kgha, color = rotation)) +
-  geom_point() + 
-  geom_smooth(method = "lm", se = F) + 
-  labs(x = "Mean site-year yield at HN",
-       y = "Yield at HN",
-       title = "7 sites, 17 years")
-         
-# does the gap go down w/higher yields? Kind of.    
-# notice it is not the same sites that yield high
-cpen %>% 
-  mutate(yldav = (cc + sc)/2) %>% 
-  ggplot(aes(yldav, cpen)) +
-  geom_point(aes(color = site), size = 3) +
-  geom_smooth(method = "lm", se = F, color = "black") + 
-  geom_hline(yintercept = 0, color = "red") + 
-  labs(x = "mean site-year yield at HN",
-       y = "cont cropping penalty, kg/ha",
-       title = "7 sites, 17 years")
-
-#--is it a sig slope?
-cpen %>% 
-  mutate(yldav = (cc + sc)/2,
-         yldavMg = yldav/1000,
-         cpenMg = cpen/1000) %>%
-  lm(cpenMg ~ yldavMg, data = .) %>% 
-  summary()
-# yes, it estimates a cpen of 2.7 Mgha at lowest yielding year
-# It goes down 142 kg per 1 Mg increase in site-year yield
-
-# yield gaps by year and site ---------------------------------------------
-
-cpen %>%
-  ggplot(aes(year, cpen)) + 
-  geom_point()
-
-cpen %>%
-  ggplot(aes(site, cpen)) + 
-  geom_point()
-
-# which has more variation? YEAR!!!!!!!!!!!!!!
-cpen %>% 
-  lmer(cpen ~ 1 + (1|site) + (1|year), data = .)
-  
-
-#--ccpenalty by site
-sad_ccpen <- sad_tidysawyer %>% 
-  group_by(crop, site, rotation) %>% 
-  mutate(nmax = max(nrate_kgha)) %>% 
-  filter(nrate_kgha == nmax) %>% 
-  select(crop, site, year, rotation, yield_kgha) %>% 
-  pivot_wider(names_from = rotation, values_from = yield_kgha) %>% 
-  mutate(ccpen = sc-cc) 
-
-
-sad_ccpen %>% 
-  ggplot(aes(reorder(site, ccpen, FUN = mean, na.rm = T), ccpen)) + 
-  geom_point() + 
-  geom_boxplot() + 
-  stat_summary(fun.y = mean, geom = "point", color = "red", size = 3) + 
-  geom_hline(yintercept = sad_ccpen %>% 
-               ungroup() %>% 
-               summarise(ccpen = mean(ccpen, na.rm = T)) %>% 
-               pull(ccpen),
-             color = "red") +
-  coord_flip()
-
-ggsave("_figs/raw_ccpen-by-site.png")
-
-sad_tidysawyer %>% 
-  group_by(crop, site, rotation) %>% 
-  mutate(nmax = max(nrate_kgha)) %>% 
-  filter(nrate_kgha == nmax) %>% 
-  select(crop, site, year, rotation, yield_kgha) %>% 
-  pivot_wider(names_from = rotation, values_from = yield_kgha) %>% 
-  mutate(ccpen = sc-cc) %>% 
-  ggplot(aes(reorder(year, ccpen, mean, na.rm = T), ccpen)) + 
-  geom_point() + 
-  stat_summary(fun.y = mean, geom = "point", color = "red", size = 3) + 
-  coord_flip()
-
