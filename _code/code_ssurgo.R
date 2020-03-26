8299897082641878f41212353d305ce45c5c5b8b
Unknown <vnichols@iastate.edu>
2020-03-26 11:33:24 -0500

    add ssurgo data (gotten via feddata pkg)

--- _code/code_ssurgo.R
+++ _code/code_ssurgo.R
@@ -1,8 +1,9 @@
 # Created:       3/25/2020
-# last edited:   3/25/2020
+# last edited:   3/26/2020 (upgrade from practice to actual lat/lon)
 # 
 # purpose: use feddata package to get ssurgo data?
 # notes: 
+
 
 rm(list = ls())
 #devtools::install_github("vanichols/saapsim", force = T)
@@ -12,6 +13,17 @@ library(janitor)
 library(lubridate)
 library(FedData)
 library(sf)
+
+
+# lat/lon each site (from saapsim pkg) ------------------------------------
+
+sll <- sad_siteinfo %>% 
+  #--this is fixed in newer pkg, delete once it's updated
+  mutate(site = str_sub(str_to_lower(site_name), 1, 4)) %>% 
+  filter(site != "kell") %>% 
+  rename(lon = long) %>%
+  #--keep this
+  select(site, lat, lon)
 
 
 #--get lat/long for site
@@ -59,107 +71,69 @@ mu_dat <- read_csv("EXTRACTIONS/CIA/SSURGO/CIA_SSURGO_mapunit.csv") %>%
   remove_empty("cols") #--removes empty columns
 
 
-# 2. chorizon 
+#--two methods:
+#1. take thing that has highest muacres (major mapunit), steal iacornsr
+
+iasr_maj <- 
+  mu_dat %>% 
+  filter(muacres == max(muacres)) %>% 
+  select(iacornsr) %>% 
+  rename(iacsr_maj = iacornsr)
+
+
+#. do a weighted average
+iasr_wgt <- 
+  mu_dat %>%
+    mutate(ac_tot = sum(muacres),
+           wgt = muacres/ac_tot,
+           val = wgt * iacornsr) %>% 
+    summarise(iacsr_wgt = sum(val))
+
+
+# 2. component, tells what each mukey is 'made up of', has mukey and cokey
+cmpnt_dat <- read_csv("EXTRACTIONS/CIA/SSURGO/CIA_SSURGO_component.csv") %>% 
+  remove_empty("cols") %>% 
+  select(mukey, cokey, everything())
+
+cmpnt_major <- 
+  cmpnt_dat %>% 
+  filter(majcompflag == "Yes") %>% 
+  select(mukey, cokey)
+
+
+# 3. chorizon 
 #  Horizon depths, sand, silt, clay, organic matter, water holding capacity, etc.
 # .r means reference value, .l means low value, .h means high value
+# we just want depth to b horizon
 chor_dat <- read_csv("EXTRACTIONS/CIA/SSURGO/CIA_SSURGO_chorizon.csv") %>% 
   remove_empty("cols")
 
-# 3. component, tells what each mukey is 'made up of' 
-cmpnt_dat <- read_csv("EXTRACTIONS/CIA/SSURGO/CIA_SSURGO_component.csv") %>% 
-  remove_empty("cols")
+ahorz_dat <- 
+  chor_dat %>% 
+  select(cokey, chkey, hzname, desgnmaster, contains(".r") & contains("hz")) %>% 
+  filter(desgnmaster == "A") %>% 
+  #--average by cokey (?) 
+  group_by(cokey) %>% 
+  summarise(depth_to_Bhz_cm = mean(hzdepb.r, na.rm = T))
 
+# 1. use major cmpnt, and major mapunit
+hzb_maj <- 
+  mu_dat %>% 
+  filter(muacres == max(muacres)) %>% 
+  left_join(cmpnt_major) %>% 
+  left_join(ahorz_dat) %>% 
+  select(depth_to_Bhz_cm) %>% 
+  rename(bhz_maj = depth_to_Bhz_cm)
 
-# Which data has info we want?
-# NOTE: mu_dat doesn't have anything we want to visualize, so we won't use it. 
+# 2. use weighted cmpnts and mapunits
 
-
-# ummm --------------------------------------------------------------------
-
-
-## Step 4A: First we simplify the data so it is easier to work with
-
-srgo_shp_simp <- 
-  srgo_shp %>% 
-  mutate(mukey = as.character(mukey)) #--we don't want it to be numeric
-
-cmpnt_simp <- cmpnt_dat %>% 
-  select(compname, comppct.r, mukey, cokey) %>% 
-  mutate(mukey = as.character(mukey)) #--again, we don't want it to be numeric
-
-chor_simp <- chor_dat %>% 
-  select(hzname, om.r, cokey, chkey) %>% #--let's just do organic matter for now
-  filter(str_detect("A", hzname)) #--let's only look at the A horizon (the top layer of soil)
-
-
-
-########################STOP#################################
-# NOTE: Why did we pick om.r? What does .r mean?
-########################STOP#################################
-
-chor_dat %>% 
-  select(hzname, om.r, om.h, om.l, cokey, chkey) %>% #--let's just do organic matter for now
-  filter(str_detect("A", hzname))
-
-
-
-srgo_shp_simp
-cmpnt_simp
-chor_simp
-
-## Step 4B: Get one organic matter value for each mukey
-
-# Combine the components with the om% data
-
-chor_cmpnt_simp <- 
-  cmpnt_simp %>% #--has components of each mukey
-  left_join(chor_simp) #--has organic matter for each component
-
-
-
-# You'll notice the comppct.r doesn't add up to 100% for each mukey
-# Let's just keep the component that is most dominant in the mukey
-
-om_simp <- 
-  chor_cmpnt_simp %>% 
-  # remember how to filter out nas?
-  filter(!is.na(chkey)) %>% 
+hzb_wgt <- 
+  cmpnt_dat %>% 
+  select(mukey, cokey, comppct.r)  %>% 
+  left_join(ahorz_dat) %>%
   group_by(mukey) %>% 
-  filter( comppct.r == max(comppct.r))
-
-
-# Next we join the above data with our spatial data
-
-new_srgo <- 
-  srgo_shp_simp %>% 
-  left_join(om_simp, by = "mukey")
-
-
-# Step 5: Visualize soils data + yield data -------------------------------
-
-# Now we can overlay the yield map data with the soils data
-
-#--Yield
-new_srgo %>% 
-  ggplot() + 
-  geom_sf(aes(fill = om.r)) +
-  scale_fill_distiller(palette = rev("YlOrBr")) +
-  geom_sf(data = yld_map, aes(color = yld_vol_dr)) + 
-  scale_color_distiller(palette = "Greens")
-
-#--Grain moisture
-new_srgo %>% 
-  ggplot() + 
-  geom_sf(aes(fill = om.r)) +
-  scale_fill_distiller(palette = rev("YlOrBr")) +
-  geom_sf(data = yld_map, aes(color = moisture)) + 
-  scale_color_distiller(palette = "Blues")
-
-
-# Note: the yield nor the grain moisture seems to be affected by the soil type. 
-# Do you remember the warning the SSURGO website gave us when we got the soils for Agronomy Hall?
-# What scale are we supposed to interpret the information at?
-# What scale are we operating at?
-# Remember 0.001 degrees is roughly 100 m
-# How many meters across is our field? 
+  mutate(comp_tot = sum(comppct.r),
+         wgt = comppct.r / comp_tot,
+         val = depth_to_Bhz_cm * wgt) %>% 
+  summarise(bhz_wt = sum(val))
 
