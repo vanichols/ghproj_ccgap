0fc24f406c58c87647b5d490082dbe11b6a0e878
vanichols <vnichols@iastate.edu>
2020-04-10 08:26:28 -0500

    add lit fig w/sawyer data included

--- code/code_lit-sum.R
+++ code/code_lit-sum.R
@@ -39,47 +39,24 @@ saw <-
   saw_tidysawyer %>%
   select(site, year, rotation, nrate_kgha, yield_kgha) %>% 
   pivot_wider(names_from = rotation, values_from = yield_kgha) %>% 
-  mutate(relative_yield = cc/sc*100) %>% 
+  mutate(relative_yield = cc/sc*100) %>%
+  filter(!is.na(relative_yield)) %>% 
   arrange(site, year, nrate_kgha) %>% 
-  mutate(years_in_corn = group_indices(., site, year),
-         scope = paste0(site, "N", round(nrate_kgha, 0)),
+  group_by(site) %>% 
+  group_modify(~{
+    .x %>% mutate(years_in_corn = group_indices(., year))
+  }) %>% 
+  mutate(scope = paste0(site),
          citation = "sawyer") %>% 
-  select(years_in_corn, relative_yield, scope)
+  ungroup() %>% 
+  select(years_in_corn, relative_yield, scope, citation, nrate_kgha)
 
-
-saw_tidysawyer %>%
-  select(site, year, rotation, nrate_kgha, yield_kgha) %>% 
-  pivot_wider(names_from = rotation, values_from = yield_kgha) %>% 
-  mutate(relative_yield = cc/sc*100) %>% 
-  arrange(site, year, nrate_kgha) %>% 
-  filter(site %in% c("ames", "nash")) %>% 
-  filter(year %in% c(2003, 2005)) %>% 
-  group_by(site, year) %>% 
-  mutate(row = row_number())
-  mutate(n = group_indices(., year))
-
-
-library(dplyr)
-dat <- tibble(site = c(rep("ames", 4), rep("nash", 4)),
-              year = c(2012, 2012, 2013, 2013, 2013, 2013, 2015, 2015))
-  
-#--desired data
-dat %>% 
-  mutate(id = c(1, 1, 2, 2, 1, 1, 2, 2))
-
-#--no 
-dat %>% 
-  mutate(n = group_indices(., year, site))
-
-#--no 
-dat %>% 
-  mutate(n = group_indices(., year))
-
-
+            
+            
+            
 # viz ---------------------------------------------------------------------
 
 dat %>%
-  bind_rows(saw) %>% 
   ggplot(aes(years_in_corn, relative_yield)) + 
   geom_point(size = 2, aes(color = scope, group = interaction(scope, citation))) + 
   geom_line(size = 2, aes(color = scope, group = interaction(scope, citation))) + 
@@ -87,9 +64,30 @@ dat %>%
   scale_x_continuous(breaks = seq(1, 10, 1)) +
   labs(x = "Years In Corn",
        y = "Yield Relative to Rotated Corn") +
+  geom_hline(yintercept = 100) +
   theme(legend.background = element_rect(color = "black"),
-        legend.position = c(0.9, 0.9),
-        legend.justification = c(1,1))
+         legend.position = c(0.9, 0.9),
+         legend.justification = c(1,1))
+
 
 ggsave("figs/lit_years-in-corn.png")
 ggsave("../../../Box/Gina_APSIM_modeling/figs-from-repo/lit_years-in-corn.png")
+
+
+dat %>%
+  bind_rows(saw) %>% 
+  ggplot(aes(years_in_corn, relative_yield)) + 
+  geom_point(size = 2, aes(color = scope, group = interaction(scope, citation))) + 
+  geom_line(size = 2, aes(color = scope, group = interaction(scope, citation))) + 
+  theme_bw() + 
+  #scale_x_continuous(breaks = seq(1, 10, 1)) +
+  labs(x = "Years In Corn",
+       y = "Yield Relative to Rotated Corn") +
+  geom_hline(yintercept = 100) +
+  theme(legend.position = "bottom") +
+  facet_grid(.~nrate_kgha)
+
+
+
+ggsave("figs/lit_years-in-corn-sawyer.png")
+ggsave("../../../Box/Gina_APSIM_modeling/figs-from-repo/lit_years-in-corn-sawyer.png")
