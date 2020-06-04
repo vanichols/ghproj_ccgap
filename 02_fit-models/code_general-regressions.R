a27e38992b27b97d969e3e733256a62e90f02582
vanichols <vnichols@iastate.edu>
2020-06-04 12:51:04 -0500

    try just step-wise elimination in regression

--- 02_fit-models/code_general-regressions.R
+++ 02_fit-models/code_general-regressions.R
@@ -1,14 +1,11 @@
-# Created:       4/1/2020
+# Created:       6/4/2020
 # 
-# purpose: stats on pct gap (rather than raw gap)
+# purpose: stats on pct gap and raw gap
 #
 # notes: 
 #
-# last edited:   4/23/2020
-#                5/20/2020 moved to 02_fit-models folder
-#               5/26/2020 use loo imp variables instead
-#               5/29/2020 do it on raw gaps instad of %
-#               6/4/2020 do a simple lm
+# last edited:   
+#
 
 rm(list = ls())
 library(tidyverse)
@@ -18,5 +15,34 @@ library(tidyverse)
 
 datraw <- read_csv("01_create-features/cf_preds-all.csv")
 
+pctgap <- datraw %>% 
+  dplyr::select(-crop, -site, -site_name, -year, -cgap_max, -iacsr) %>% 
+  filter(years_in_corn > 1)
 
- 
+rawgap <- datraw %>% 
+  dplyr::select(-crop, -site, -site_name, -year, -cgap_max_pct, -iacsr) %>% 
+  filter(years_in_corn > 1)
+
+
+# simple regression -------------------------------------------------------
+#library(MASS) #--messes up select, has stepAIC but don't know diff btwn that and step
+#--no interactions
+
+#--with %cgap as response
+m1 <- lm(cgap_max_pct ~ ., data = pctgap)
+sm1 <- step(m1) #--gives me an error about number of rows? there aren't any NAs...
+summary(sm1)
+
+#--with rawgap as response
+m2 <- lm(cgap_max ~ ., data = rawgap %>% dplyr::select(-yearF))
+sm2 <- stepAIC(m1) #--gives me an error about number of rows? there aren't any NAs...
+summary(sm1)
+
+
+# no interactions
+m1 <- lm(cgap_max_pct ~ ., data = pctgap %>% dplyr::select(-yearF))
+sm1 <- stepAIC(m1) #--gives me an error about number of rows? there aren't any NAs...
+summary(sm1)
+
+m2 <- lm(cgap_max_pct ~ .*., data = pctgap %>% select(-yearF))
+step(m2)
