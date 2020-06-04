d578d02977a47c8cf31ecc584f773efd53d9bbc0
vanichols <vnichols@iastate.edu>
2020-06-04 12:51:14 -0500

    weather pca?

--- 01_create-features/code_preds-wea.R
+++ 01_create-features/code_preds-wea.R
@@ -30,7 +30,8 @@ library(tidyr)
 
 wea <- saw_wea %>% as_tibble() 
 
-pwea <- wea %>% 
+pwea <- 
+  wea %>% 
   left_join(saw_planting) %>% 
   select(-plant_date, -crop) %>% 
   filter(!is.na(plant_doy))
@@ -40,6 +41,37 @@ pwea %>%
   select(site, year) %>% 
   distinct() 
 
+
+# do a pca? ---------------------------------------------------------------
+
+library("factoextra")
+data(decathlon2)
+decathlon2.active <- decathlon2[1:23, 1:10]
+head(decathlon2.active[, 1:6])
+res.pca <- prcomp(decathlon2.active, scale = TRUE)
+fviz_eig(res.pca)
+fviz_pca_ind(res.pca,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+fviz_pca_var(res.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+fviz_pca_biplot(res.pca, repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
+
+# #-bahhhhhh
+# pwea %>% 
+#   unite(site, year, col = "site_year") %>%
+#   
+#   column_to_rownames("site_year_day")
+# 
+# w.pca <- prcomp(pwea, scale = TRUE)
 
 # weather in reference to planting ----------------------------------------
 
@@ -298,6 +330,30 @@ corrplot::corrplot(corres)
 ggsave("01_create-features/fig_wea-corrs.png")
 
 # i like heatstress more than gs_tavg I think, comment out line 272
+
+wea_pca <- 
+  wea_parms %>% 
+  unite(site, year, col = "site_year") %>% 
+  column_to_rownames("site_year")
+
+res_pca <- prcomp(wea_pca, scale = TRUE)
+fviz_eig(res_pca)
+fviz_pca_ind(res_pca,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+fviz_pca_var(res_pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+fviz_pca_biplot(res_pca, repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
+
+
 
 # write what i like -------------------------------------------------------
 
