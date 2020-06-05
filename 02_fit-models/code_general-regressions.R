8a6a2f291b13bba42e8eededdf510515e2b3d828
vanichols <vnichols@iastate.edu>
2020-06-04 19:57:22 -0500

    now regression only picks 2 wk precip

--- 02_fit-models/code_general-regressions.R
+++ 02_fit-models/code_general-regressions.R
@@ -13,34 +13,61 @@ library(tidyverse)
 
 # data --------------------------------------------------------------------
 
-datraw <- read_csv("01_create-features/cf_preds-all.csv")
-
-
-
-mdat <- datraw %>% 
-  dplyr::select(-crop, -site, -site_name, -year, -yearF, -iacsr) %>% 
+datraw <- read_csv("01_create-features/cf_preds-all.csv") %>% 
   filter(years_in_corn > 1)
 
+
+datraw %>% 
+  filter(year > 2005, year < 2016) %>% 
+  ggplot(aes(reorder(year, cgap_max, mean), cgap_max)) + 
+  geom_point(aes(color = site, size = p2wk_precip_mm_tot)) + 
+  #facet_grid(.~drainage) +
+  coord_flip() 
+
+
+datraw %>% 
+  filter(year > 2005, year < 2016) %>% 
+  ggplot(aes(reorder(year, cgap_max, mean), cgap_max)) + 
+  geom_point(aes(color = site, size = wyprecip_mm)) + 
+  #facet_grid(.~drainage) +
+  coord_flip() 
+
+#--correct yield?
+datraw %>% 
+#  filter(year > 2005, year < 2016) %>% 
+  mutate(yield_corrected = cgap_max - 0.605*years_in_corn) %>% 
+  ggplot(aes(reorder(year, yield_corrected, mean), yield_corrected)) + 
+  geom_jitter(aes(color = site, size = p2wk_precip_mm_tot)) + 
+  coord_flip()
+
+  
+
+mdat <- datraw %>% 
+  dplyr::select(-crop, -site_name, -year, -yearF, -iacsr) 
 
 # simple regression -------------------------------------------------------
 #library(MASS) #--messes up select, has stepAIC but don't know diff btwn that and step
 #--no interactions
 
 #--with %cgap as response
-m1 <- lm(cgap_max_pct ~ ., 
-         data = mdat %>% select(-cgap_max))
-sm1 <- step(m1, ,
-            k = log(nrow(pctgap))) #--gives BIC instead of AIC, this seriously limits what gets in
-
-m1vars <- sm1$coefficients %>% enframe() %>% filter(name != "(Intercept)")
-
-summary(sm1)
-anova(sm1)
+# m1 <- lm(cgap_max_pct ~ ., 
+#          data = mdat %>% select(-cgap_max, -soc_30cm_pct))
+# sm1 <- step(m1, ,
+#             k = log(nrow(mdat))) #--gives BIC instead of AIC, this seriously limits what gets in
+# 
+# m1vars <- sm1$coefficients %>% enframe() %>% filter(name != "(Intercept)")
+# 
+# summary(sm1)
+# anova(sm1)
 
 #--with rawgap as response
-m2 <- lm(cgap_max ~ ., data = rawgap %>% dplyr::select(-yearF))
-sm2 <- step(m2)
+m2 <- lm(cgap_max ~ ., 
+         data = mdat %>% dplyr::select(-cgap_max_pct))
+         
+sm2 <- step(m2, k = log(nrow(mdat)))
 summary(sm2)
+
+m2vars <- sm2$coefficients %>% enframe()
 
 #resid(sm2)
 library(broom)
@@ -63,7 +90,10 @@ summary(sm1a) %>%
 
 #--on raw diff
 m1a <- lm(cgap_max ~ .*., 
-          data = pctgap %>% select_at(vars(m1vars$name, cgap_max_pct)))
+          data = mdat %>% select(-cgap_max_pct))
+# 
+# m1a <- lm(cgap_max ~ .*., 
+#           data = pctgap %>% select_at(vars(m1vars$name, cgap_max_pct)))
 sm1a <- step(m1a)
 sm1a
 summary(sm1a) %>% 
