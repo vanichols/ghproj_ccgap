acbdfd1c804613f80e7868a50dc795ee86976f24
vanichols <vnichols@iastate.edu>
2020-11-19 08:41:08 -0600

    add erikson data

--- 00_exp-explore/code_lit-summary.R
+++ 00_exp-explore/code_lit-summary.R
@@ -75,6 +75,72 @@ ggsave("00_exp-explore/fig_lit-pen-over-time-v2.png")
 ggsave("../../../Box/Gina_APSIM_modeling/docs/fig_lit-pen-over-time-v2.png")
 
 
+# try adding erikson ------------------------------------------------------
+
+
+dat_me <- 
+  read_excel("00_exp-explore/lit_summary-penalty-over-years.xlsx", sheet = "over-years") %>% 
+  fill(scope) %>%  
+  mutate(cit_id = paste0(citation, scope))
+
+
+dat_erik <- 
+  read_csv("00_exp-explore/lit-erikson-summary.csv") %>% 
+  clean_names() %>% 
+  filter(!is.na(years)) %>% 
+  mutate(ccpen = parse_number(continuous_corn_vs_rotation)) %>%
+  separate(location, into = c("city", "state"), sep = ",") %>% 
+  mutate(state = str_trim(state)) %>% 
+  filter(!state %in% c("AL", "TN", "NY", "KY", "NJ")) %>% 
+  mutate(relative_yield = 100 + ccpen,
+         years_in_corn = 2) %>% 
+  rename(citation = primary_author_year_published_see_full_citations_below) %>% 
+  mutate(cit_id = paste0(citation, state))
+
+
+dat <- 
+  dat_me %>% 
+  select(cit_id, citation, years_in_corn, relative_yield) %>% 
+  bind_rows(dat_erik %>% 
+              select(cit_id, citation, years_in_corn, relative_yield))
+
+# viz nice ---------------------------------------------------------------------
+
+library(scales)
+
+theaverage <- 
+  dat %>%
+  filter(!grepl("Gentry|Seifert", citation)) %>% 
+  filter(years_in_corn > 1) %>% 
+  summarise(mn_rel_yield = mean(relative_yield)) %>% 
+  pull()/100
+
+#--this is ugly
+
+dat %>%
+  mutate(cit_star = ifelse(grepl("Gentry|Seifert", citation), "*", " "),
+         citation = paste0(citation, cit_star)) %>% 
+  ggplot(aes(years_in_corn-1, relative_yield/100)) + 
+  geom_hline(yintercept = theaverage, linetype = "dotted", size = 1) +
+  geom_point(size = 2, aes(color = cit_star, group = cit_id)) + 
+  geom_line(aes(color = cit_star, group = cit_id)) + 
+  geom_text(x = -0.1, y = 0.81, label = "*Not replicated field trials, excluded from mean calculation",
+            hjust = 0, fontface = "italic") +
+  
+  guides(color = F) +
+  labs(x = "Years In Continuous Corn",
+       y = "Yield Relative to Rotated Corn", 
+       color = NULL) +
+  
+  scale_color_manual(values = c("red3", "gray50")) +
+  scale_x_continuous(breaks = seq(0, 10, 1)) +
+  scale_y_continuous(labels = scales::label_percent(accuracy = 1)) +
+  
+  theme_bw() + 
+  theme(legend.background = element_rect(color = "black"),
+        legend.position = c(0.9, 0.9),
+        legend.justification = c(1,1))
+
 ############ OLD CODE ###################
 
 
