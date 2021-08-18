bca4eadc09b541b9263f4bd65d1ceaa5dcf2982e
vanichols <vnichols@iastate.edu>
2021-08-18 11:54:41 -0500

    tweaking figs

--- 05_talk-figs/code_gap-conceptual.R
+++ 05_talk-figs/code_gap-conceptual.R
@@ -241,3 +241,52 @@ ggplot() +
 ggsave("05_talk-figs/fig_concep1-no-aonr.png", width = 8.73, height = 4.7)
 
 
+
+
+# labelled as ames 2008 ---------------------------------------------------------
+
+
+ggplot() + 
+  geom_line(data = viz.prds, aes(x = nrate_kgha, y = pred_yield/1000, color = rot), size = 2) + 
+  geom_point(data = viz.aonr, aes(x = nrate_kgha, y = pred_yield/1000, fill = rot), pch = 23, size = 4, stroke = 2) + 
+  #--arrows
+  geom_segment(aes(x = 310, xend = 330, 
+                   y = sc_1/1000, yend = sc_1/1000), 
+               size = 1.2, arrow = arrow(length = unit(0.2,"cm")), color = "gray80") +
+  geom_segment(aes(x = 310, xend = 330, 
+                   y = cc_2/1000, yend = cc_2/1000), 
+               size = 1.2, arrow = arrow(length = unit(0.2,"cm")), color = "gray80") +
+  #--rectangles
+  geom_rect(aes(xmin = 340, xmax = 360,
+                ymin = cc_2/1000, ymax = sc_2/1000),
+            fill = clr_red, color = "black") +
+  geom_text(aes(x = 330, y = ((sc_2 + cc_2)/2)/1000+0.3, 
+                label = "Continuous corn penalty"),
+            hjust = 1, fontface = "italic", size = 7) +
+  geom_text(aes(x = 330, y = ((sc_2 + cc_2)/2)/1000-0.3, 
+                label = paste(expression(2~Mg~ha^-1))),
+            hjust = 1, fontface = "italic", size = 7, parse = T) +
+  scale_color_manual(values = c("Continuous corn AONR" = clr_cc, 
+                                "Rotated corn AONR" = clr_rot)) +
+  scale_fill_manual(values = c("Continuous corn AONR" = clr_cc, 
+                               "Rotated corn AONR" = clr_rot)) +
+  labs(x = expression(Nitrogen~fertilization~rate~(kg~N~ha^{-1})),
+       y = yld_lab,
+       color = NULL,
+       fill = NULL,
+       title = "Ames, 2008") +
+  scale_x_continuous(limits = c(0, 360)) +
+  theme_bw() + 
+  theme(legend.position = c(0.95, 0.05),
+        legend.justification = c(1, 0),
+        legend.background = element_rect(color= "black"),
+        axis.title.y = element_text(angle = 0, vjust = 0.5),
+        legend.text = element_text(size = rel(1.5)),
+        axis.text = element_text(size = rel(1.4)),
+        axis.title = element_text(size =rel(1.5)),
+        panel.grid = element_blank(),
+        plot.title = element_text(size = rel(2)))
+
+
+ggsave("05_talk-figs/fig_concep-ames.png", width = 7.26, height = 4.76)
+
